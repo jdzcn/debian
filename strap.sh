c0c52482914e996d63fad1fc5bfb3cc2badd6c36
sb <jdzcn@qq.com>
2020-12-27 14:44:53 +0800

    add

---
+++ strap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#apt install debootstrap -y
+debootstrap buster /mnt http://mirrors.tuna.tsinghua.edu.cn/debian
+
+echo 'debian' >/mnt/etc/hostname
+echo '127.0.0.1 debian.localdomain debian'>>/mnt/etc/hosts
+cp interfaces /mnt/etc/network
+
+cd /mnt
+mount --bind /dev dev
+mount --bind /dev/pts dev/pts
+mount --bind /proc proc
+mount --bind /sys sys
+
+chroot /mnt
+
+#echo 'UUID=*** / ext4 defaults 0 1' >/mnt/etc/fstab
+#apt install linux-image-amd64
+#apt install grub-pc
